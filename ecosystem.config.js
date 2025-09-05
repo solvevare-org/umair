d560c7a23f630f7fa8247772bd823ecfb890defa
SyedAshar7 <fawaazashar@gmail.com>
2025-09-06 03:56:42 +0500

    Sufyan instructions

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -14,5 +14,20 @@ module.exports = {
     out_file: "../logs/backend-out.log",
     log_file: "../logs/backend-combined.log",
     time: true
+  }, {
+    name   : "umair-frontend",
+    script : "openai.js",
+    cwd    : "./frontend",
+    watch  : false,
+    instances: 1,
+    exec_mode: "fork",
+    env    : {
+      "NODE_ENV": "production",
+      "PORT": 3002
+    },
+    error_file: "../logs/frontend-error.log",
+    out_file: "../logs/frontend-out.log",
+    log_file: "../logs/frontend-combined.log",
+    time: true
   }]
 }
