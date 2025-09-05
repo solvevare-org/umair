9d31ae0f6fa0af98cd4f5ab9db7067644e593e74
SyedAshar7 <fawaazashar@gmail.com>
2025-09-06 02:48:47 +0500

    ubuntu compatibility

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   apps : [{
     name   : "umair-backend",
-    script : "./backend/server.js",
-    cwd    : "./",
+    script : "server.js",
+    cwd    : "./backend",
     watch  : false,
     instances: 1,
     exec_mode: "fork",
@@ -10,14 +10,13 @@ module.exports = {
       "NODE_ENV": "production",
       "PORT": 3004
     },
-    error_file: "./logs/backend-error.log",
-    out_file: "./logs/backend-out.log",
-    log_file: "./logs/backend-combined.log",
+    error_file: "../logs/backend-error.log",
+    out_file: "../logs/backend-out.log",
+    log_file: "../logs/backend-combined.log",
     time: true
   }, {
     name   : "umair-frontend",
-    script : "node",
-    args   : "openai.js",
+    script : "openai.js",
     cwd    : "./frontend",
     watch  : false,
     instances: 1,
